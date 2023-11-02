bbbb21a127098dfa5f96662ab430b1f69c06b038
chathushkasavinda <chathushkasavinda18@gmail.com>
2023-11-02 22:35:28 +0530

    content updated

--- BackEnd/src/main/java/lk/easy/car_rental/controller/CustomerController.java
+++ BackEnd/src/main/java/lk/easy/car_rental/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package lk.easy.car_rental.controller;
 
+import lk.easy.car_rental.dto.CustomerDTO;
 import lk.easy.car_rental.service.CustomerService;
 import lk.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,17 @@ public class CustomerController {
     @Autowired
      CustomerService customerService;
 
-    //@PostMapping
+    @PostMapping
+    public ResponseUtil saveCustomer(@RequestBody CustomerDTO customerDTO){
+        customerService.saveCustomer(customerDTO);
+        return new ResponseUtil("OK","Successfully Saved!!","");
+
+    }
     @GetMapping
     public ResponseUtil getAll(){
         return new ResponseUtil("OK","Successfully Saved...!!",customerService.getAllCustomer());
     }
+
+
+
 }
